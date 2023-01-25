ea3de756de2b5c6abc2c2462b1c48fe1dd492048
Silver <silver.guo@wizvision.com>
2023-01-25 14:50:57 +0800

    initial commit

---
+++ src/test/java/qp/scs/ScsAdminWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package qp.scs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsAdminWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
